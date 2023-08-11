a3498f535603f2047df324710bdc884c7253740c
Daniel Pakoca <danielpakoca@gmail.com>
2023-08-11 19:08:35 +0200

    Getting data from Mocki

---
+++ src/api/useData.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+const useData = (url) => {
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    const dataFetch = async () => {
+      const data = await (await fetch(url)).json();
+
+      setState(data);
+    };
+
+    dataFetch();
+  }, [url]);
+
+  return { data: state };
+};
+
+export default useData;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+
+import useData from "../api/useData.js";
+
 function Home() {
-  return <div>Home Page</div>;
+  const { data } = useData(
+    "https://mocki.io/v1/c9d2a9d5-56a2-42e3-aece-53ca9682f254"
+  );
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    if (data?.products) {
+      setProducts(data?.products);
+    }
+  }, [data]);
+
+  if (!products) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {products.map((product) => {
+        return <div key={product.name}>{product.name}</div>;
+      })}
+    </div>
+  );
 }
 
 export default Home;
